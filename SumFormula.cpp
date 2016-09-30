ff7e2b02c660d9b2ae8c60a58ff97707fbea585f
Ettore <ettorecelozzi@gmail.com>
2016-09-30 11:05:56 +0200

    classe sumformula

---
+++ SumFormula.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Michele on 30/09/2016.
+//
+
+#include "SumFormula.h"
---
+++ SumFormula.h
@@ -0,0 +1,14 @@
+//
+// Created by Michele on 30/09/2016.
+//
+
+#ifndef ESAMELABORATORIO_SUMFORMULA_H
+#define ESAMELABORATORIO_SUMFORMULA_H
+
+
+class SumFormula {
+
+};
+
+
+#endif //ESAMELABORATORIO_SUMFORMULA_H
